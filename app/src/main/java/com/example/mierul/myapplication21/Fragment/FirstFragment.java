9b0108c0c1ff95faf9a84e85f0f1561860e0e706
HEXA-AMIERULJAP\Hexa-Amierul.Japri <amierul.japri@hexainitiative.com>
2017-05-12 16:21:05 +0800

    divider

--- app/src/main/java/com/example/mierul/myapplication21/Fragment/FirstFragment.java
+++ app/src/main/java/com/example/mierul/myapplication21/Fragment/FirstFragment.java
@@ -4,19 +4,16 @@ import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -30,16 +27,14 @@ import android.widget.Toast;
 
 import com.example.mierul.myapplication21.Base.BaseFragment;
 import com.example.mierul.myapplication21.FirebaseHelper;
-import com.example.mierul.myapplication21.GridDividerItemDecoration;
 import com.example.mierul.myapplication21.GridSpacingItemDecoration;
 import com.example.mierul.myapplication21.Model.ProductModel;
 import com.example.mierul.myapplication21.ItemClickSupport;
 import com.example.mierul.myapplication21.Model.ProfileFirebaseModel;
 import com.example.mierul.myapplication21.R;
 import com.example.mierul.myapplication21.Adapter.ProductAdapter;
-import com.example.mierul.myapplication21.SimpleDividerItemDecoration;
-import com.example.mierul.myapplication21.SpacesItemDecoration;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,7 +49,6 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
 
     private static final String TAG = "FirstFragment";
 
-
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,12 +59,10 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
     }
 
     private void getData() {
-
         item = new ArrayList<>();
         AssetManager assetmanager = getActivity().getAssets();
 
         try {
-
             String[] imgPath = assetmanager.list("img");
             for(String path: imgPath){
                 InputStream is = assetmanager.open("img/"+path);
@@ -79,8 +71,8 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
                 item.add(new ProductModel(path,bitmap));
             }
 
-        } catch (Exception e){
-            Log.e(TAG,"getData",e);
+        } catch (IOException ioe){
+            Log.e(TAG,"getData",ioe);
         }
     }
 
@@ -103,22 +95,13 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
         setupDrawerContent(navigationView);
 
         int numColumns = 2;
-//        Drawable horizontalDivider = ContextCompat.getDrawable(getContext(), R.drawable.divider_horizontal);
-//        Drawable verticalDivider = ContextCompat.getDrawable(getContext(), R.drawable.divider_vertical);
 
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.productRecyclerView);
-//        StaggeredGridLayoutManager gridLayoutManager =
-//                new StaggeredGridLayoutManager(numColumns, StaggeredGridLayoutManager.VERTICAL);
-//        recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(),numColumns));
-        //recyclerView.addItemDecoration(new GridSpacingItemDecoration(numColumns, dpToPx(10), true));
-        //recyclerView.addItemDecoration(new GridDividerItemDecoration(horizontalDivider, verticalDivider, numColumns));
-        //recyclerView.addItemDecoration(new SimpleDividerItemDecoration(getContext()));
-        //recyclerView.addItemDecoration(new SpacesItemDecoration(numColumns));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(numColumns, dpToPx(20), true));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         ProductAdapter adapter = new ProductAdapter(item);
         recyclerView.setAdapter(adapter);
-
 
         ItemClickSupport.addTo(recyclerView).setOnItemClickListener(this);
 
@@ -128,14 +111,18 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
         return view;
     }
 
+    //Converting dp to pixel
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
+
     private void setupHeaderLayout(View headerLayout) {
 
         View imageHeader =headerLayout.findViewById(R.id.navigation_header_imageView);
-
         imageHeader.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 boolean auth = helper.isLogin();
                 ProfileFirebaseModel model = helper.getProfile();
 
@@ -231,13 +218,5 @@ public class FirstFragment extends BaseFragment implements ItemClickSupport.OnIt
 
     public boolean isDrawerOpen(){
         return drawer.isDrawerOpen(Gravity.START);
-    }
-
-    /**
-     * Converting dp to pixel
-     */
-    private int dpToPx(int dp) {
-        Resources r = getResources();
-        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 }
