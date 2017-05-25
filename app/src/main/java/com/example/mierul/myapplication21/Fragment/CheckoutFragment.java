4755a7c4de002f0fac06890a8640e4cec740d4fa
sasuketaichou <sasuketaichou@gmail.com>
2017-05-25 23:47:28 +0800

    add checkout divider

--- app/src/main/java/com/example/mierul/myapplication21/Fragment/CheckoutFragment.java
+++ app/src/main/java/com/example/mierul/myapplication21/Fragment/CheckoutFragment.java
@@ -2,6 +2,7 @@ package com.example.mierul.myapplication21.Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -50,9 +51,11 @@ public class CheckoutFragment extends BaseFragment implements View.OnClickListen
         View view = inflater.inflate(R.layout.fragment_checkout,container,false);
 
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.checkOutRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
         adapter = new CheckoutAdapter(getContext(),item);
         recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(),layoutManager.getOrientation()));
 
         return view;
     }
